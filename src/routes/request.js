5b775d8dfc5104fb70c4e2b074a90180cdb8c516
Kaustubh070707 <k9523604@gmail.com>
2025-01-02 12:57:03 +0530

    Added the Connection Request api

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,6 +3,7 @@ const requestRouter = express.Router();
 
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 requestRouter.post(
   "/request/send/:status/:toUserId",
@@ -12,6 +13,26 @@ requestRouter.post(
       const fromUserId = req.user._id;
       const toUserId = req.params.toUserId;
       const status = req.params.status;
+
+      const allowedStatus = ["intrested", "ignored"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Invalid status : " + status });
+      }
+      const existingConnectionRequest = await ConnectionRequest.findOne({
+        $or: [
+          { fromUserId, toUserId },
+          { fromUserId: toUserId, toUserId: fromUserId },
+        ],
+      });
+      if (existingConnectionRequest) {
+        return res
+          .status(400)
+          .send({ message: "Connection request already exists" });
+      }
+      const toUser = await User.findOne({ _id: toUserId });
+      if (!toUser) {
+        return res.status(404).json({ message: "User does not exist" });
+      }
       const connectionRequest = new ConnectionRequest({
         fromUserId,
         toUserId,
@@ -25,7 +46,6 @@ requestRouter.post(
     } catch (err) {
       res.status(404).send(err.message);
     }
-    res.send(user.firstName + " sent a connection request");
   }
 );
 
